9d9d7cf752142d0f6c4cf05a80863453d6400d84
jishnupramod <jishnumagnanimous@gmail.com>
2019-09-04 23:24:23 +0530

    Integrate Calendar API

---
+++ main.py
@@ -0,0 +1,87 @@
+from __future__ import print_function
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+import os
+import time
+import playsound
+import speech_recognition as sr
+from gtts import gTTS
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+def speak(text):
+    tts = gTTS(text=text, lang="en")
+    filename = 'voice.mp3'
+    tts.save(filename)
+    playsound.playsound(filename)
+
+def get_audio():
+    r =sr.Recognizer()
+    with sr.Microphone() as source:
+        audio = r.listen(source)
+        speech = ""
+
+    try:
+        speech = r.recognize_google(audio)
+        print(speech)
+    except Exception as e:
+        print("Exception: " + e)
+
+    return speech
+
+# speak("Hello, What's your name ?")
+# name = get_audio()
+# speak("Nice meeting you " + name)
+
+
+def authenticate_google():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+    return service
+
+
+def get_events(n, service):
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    speak(f'Getting the upcoming {n} events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=n, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        speak('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        speak(f'The event {event["summary"]} is there on {start}')
+
+
+service = authenticate_google()
+get_events(2, service)
